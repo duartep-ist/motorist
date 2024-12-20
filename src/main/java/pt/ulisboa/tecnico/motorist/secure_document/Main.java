4f8fd06203799cc066c90e110ebf81d77044e107
Duarte Pardal <duarte.pardal@tecnico.ulisboa.pt>
2024-12-20 19:18:11 +0000

    Separate secure document code into library and tool

---
+++ src/main/java/pt/ulisboa/tecnico/motorist/secure_document/Main.java
@@ -0,0 +1,71 @@
+package pt.ulisboa.tecnico.motorist.secure_document;
+
+import pt.ulisboa.tecnico.motorist.common.SecureDocument;
+
+public class Main {
+	// Usage example for testing:
+	// Help:
+	// mvn -q exec:java -Dexec.mainClass="pt.ulisboa.tecnico.motorist.common.SecureDocument" -Dexec.args="help"
+	// Generate Dummy key:
+	// mvn -q exec:java -Dexec.mainClass="pt.ulisboa.tecnico.motorist.common.SecureDocument" -Dexec.args="generate-key keyfile.key"
+	// Protect:
+	// mvn -q exec:java -Dexec.mainClass="pt.ulisboa.tecnico.motorist.common.SecureDocument" -Dexec.args="protect example.json keyfile.key protected_example.dat"
+	// Check:
+	// mvn -q exec:java -Dexec.mainClass="pt.ulisboa.tecnico.motorist.common.SecureDocument" -Dexec.args="check protected_example.dat keyfile.key"~
+	// Unprotect:
+	// mvn -q exec:java -Dexec.mainClass="pt.ulisboa.tecnico.motorist.common.SecureDocument" -Dexec.args="unprotect protected_example.dat keyfile.key unprotected_example.json"
+	
+	/**
+	 * Command-line interface
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			printHelp();
+			return;
+		}
+
+		String command = args[0];
+		try {
+			switch (command) {
+				case "help":
+					printHelp();
+					break;
+				case "protect":
+					if (args.length != 4) throw new IllegalArgumentException("Invalid arguments for protect.");
+					SecureDocument.protect(args[1], args[2], args[3]);
+					break;
+				case "check":
+					if (args.length != 3) throw new IllegalArgumentException("Invalid arguments for check.");
+					SecureDocument.check(args[1], args[2]);
+					break;
+				case "unprotect":
+					if (args.length != 4) throw new IllegalArgumentException("Invalid arguments for unprotect.");
+					SecureDocument.unprotect(args[1], args[2], args[3]);
+					break;
+				case "generate-key":
+					if (args.length != 2) throw new IllegalArgumentException("Invalid arguments for generate-key.");
+					SecureDocument.generateDummyKey(args[1]);
+					break;
+				default:
+					throw new IllegalArgumentException("Unknown command: " + command);
+			}
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	* Usage instructions
+	*/
+	private static void printHelp() {
+		System.out.println("Usage:");
+		System.out.println(" help                             - Print this help message");
+		System.out.println(" protect <input> <key> <output>   - Protect a document");
+		System.out.println(" check <input> <key>              - Check the integrity of a document");
+		System.out.println(" unprotect <input> <key> <output> - Unprotect a document");
+		System.out.println(" generate-key <keyfile>           - Generate a dummy key");
+	}
+}
